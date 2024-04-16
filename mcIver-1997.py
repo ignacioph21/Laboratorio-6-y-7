fc2e939b4c06e530a0101acb74cf4d5f2f08ed7c
valencincu <83369023+valencincu@users.noreply.github.com>
2024-04-16 17:11:42 -0300

    Modificaciones al código de mcIver-1997 (como extraer contours de Matplotlib)

--- mcIver-1997.py
+++ mcIver-1997.py
@@ -29,22 +29,12 @@ def integrando2(nu, r, y):
     return (nu*np.sin(nu*y) + np.cos(nu*y))*sp.iv(0,nu*c)*sp.kv(1,nu*r)*nu/(nu**2+1)
 
 def integral1(r, y):  
-    sol = np.zeros(np.shape(r))
-    for i in range(len(r[:,0])):
-        for j in range(len(r[0,:])):
-            #print(np.shape(quad(integrando1, 0, a, args=(r[i,j], y[i,j]))))
-            sol[i,j] = quad(integrando1, 0, a, args=(r[i,j], y[i,j]))[0]
-    return sol        
+    return quad(integrando1, 0, a, args=(r, y))[0]        
 
 def integral2(r, y):
-    sol = np.zeros(np.shape(r))
-    for i in range(len(r[:,0])):
-        for j in range(len(r[0,:])):
-            sol[i,j] = quad(integrando2, 0, a, args=(r[i,j], y[i,j]))[0]
-    return sol        
+    return quad(integrando2, 0, a, args=(r, y))[0]        
 
 def psi1(r,y):
-    print(np.shape(r), np.shape(y))
     M = lambda r,y: r*np.exp(-y)*sp.jv(1,r)*sp.hankel1(0,c)
     return -4*np.pi**2*1j*c*M(r,y) - 8*c*r * integral1(r,y) + L(np.abs(r-c),y,a) / (2*np.sqrt(r*c))
 
@@ -53,17 +43,29 @@ def psi2(r,y):
     return -4*np.pi**2*1j*c*M(r,y) + 8*c*r * integral2(r,y) + L(np.abs(r-c),y,a) / (2*np.sqrt(r*c))
 
 def psi(r,y):
-    #return np.concatenate(psi1(r[r<c],y), psi2(r[r>=c],y))
-    return psi1(r,y)*(r<c) + psi2(r,y)*(r>=c)
+    sol = np.zeros(np.shape(r))
+    for i in range(len(r[:,0])):
+        for j in range(len(r[0,:])):
+            sol[i,j] = psi1(r[i,j],y[i,j])[0] if (r[i,j]<c) else psi2(r[i,j],y[i,j])[0]
+    return sol
 
-r = np.linspace(0, 10, 100)
-y = np.linspace(-1, 5, 100)
+r = np.linspace(0, 10, 25)
+y = np.linspace(0, 5, 25)
 
 R, Y = np.meshgrid(r,y)
 
 plt.gca().set_aspect('equal')
-plt.contour(R, Y, np.real(psi(R,Y)), levels = [20, 16, 12, 8, 4][::-1])
+cs = plt.contour(R, Y, np.real(psi(R,Y)), levels = [20, 16, 12, 8, 4][::-1])
 plt.hlines(0, 0, 10)
 plt.gca().invert_yaxis()
 
+plt.close()
+
+# Como extraer contours con matplotlib:
+p = cs.collections[0].get_paths()[0]
+v = p.vertices
+x = v[:,0]
+y = v[:,1]
+
+plt.plot(x, y)
 plt.show()
